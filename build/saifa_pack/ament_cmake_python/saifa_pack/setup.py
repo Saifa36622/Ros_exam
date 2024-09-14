d484614451727c38fcff30604b3df7a8561a323d
Bullford_G <saifa36622@gmail.com>
2024-09-15 00:56:00 +0700

    last_i_guest

--- build/saifa_pack/ament_cmake_python/saifa_pack/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages
-from setuptools import setup
-
-setup(
-    name='saifa_pack',
-    version='0.0.0',
-    packages=find_packages(
-        include=('saifa_pack', 'saifa_pack.*')),
-)
--- install/sun_interfaces/local/lib/python3.10/dist-packages/sun_interfaces/srv/__init__.py
+++
@@ -1,1 +0,0 @@
-from sun_interfaces.srv._pizza_pose import PizzaPose  # noqa: F401
