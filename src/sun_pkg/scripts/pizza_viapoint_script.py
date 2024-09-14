171ba650cd1ad5bf15d6f566f82e698f1f248eb8
SunnY <93282477+nakerin7588@users.noreply.github.com>
2024-09-14 20:56:37 +0700

    Update teleop_turtle and pizza viapoint

--- src/sun_pkg/scripts/pizza_viapoint_script.py
+++ src/sun_pkg/scripts/pizza_viapoint_script.py
@@ -7,15 +7,28 @@ import os
 
 # Additional interfaces
 from sun_interfaces.srv import PizzaPose
+from turtlesim_plus_interfaces.srv import GivePosition
+from rcl_interfaces.msg import SetParametersResult
+from std_srvs.srv import Trigger
 
 class PizzaViapointNode(Node):
     def __init__(self):
         super().__init__('pizza_viapoint_node')
         
+        # Declare parameters
+        self.declare_parameter('max_pizza', 20)
+        self.declare_parameter('name', 't_name')
+        
+        self.max_pizza = self.get_parameter('max_pizza').get_parameter_value().integer_value
+        
         # Create the service server
-        self.save_pizza_server = self.create_service(PizzaPose, 'save_pizza', self.save_pizza_callback)
-        self.get_logger().info('Service server ready to receive 1D array of float64.')
+        self.ready_to_spawn_pizza = self.create_service(GivePosition, 'ready_to_spawn_pizza', self.spawn_pizza_request)
+        self.save_pizza_server = self.create_service(Trigger, 'save_pizza', self.save_pizza_callback)
+        self.get_logger().info('Service server ready to receive pizza position.')
 
+        # Create the service client
+        self.spawn_pizza_client = self.create_client(GivePosition, 'spawn_pizza')
+        
         # Set path of .yaml file
         self.yaml_file_path = os.path.join(os.path.dirname(__file__), '../pizza_pose/pizza_pose.yaml')
 
@@ -24,24 +37,54 @@ class PizzaViapointNode(Node):
         if not os.path.exists(directory):
             os.makedirs(directory)
             self.get_logger().info(f"Directory created: {directory}")
+            
+        # Additional variables
+        self.count_pizza = 0
+        self.keep_pose = []
+        self.update_number = 0
+        
+        # Add a callback to dynamically update parameters
+        self.add_on_set_parameters_callback(self.on_parameter_update)
+    
+    def spawn_pizza_request(self, request, response):
+        
+        if self.count_pizza < self.max_pizza:
+            self.count_pizza += 1
+            position_request = GivePosition.Request()
+            position_request.x = request.x
+            position_request.y = request.y
+            self.keep_pose.append([position_request.x, position_request.y])
+            self.spawn_pizza_client.call_async(position_request)
+            self.get_logger().info(f'Pizza spawn: {self.count_pizza} pcs')
+            return response
+        else :
+            self.get_logger().warning(f'Just spawn pizza only {self.max_pizza} mother fucker')
+            return response
+        
     
     def save_pizza_callback(self, request, response):
-        temp = []
-        # Handle the incoming 1D array of float64
-        self.get_logger().info(f"Received pizza pose to save to pizza_pose.yaml")
         
-        for i in range(len(request.x)):
-            temp.append([request.x[i],request.y[i]])
-        
-        print(temp, request.number)
-        
-        # Call the function to append data to YAML
-        self.append_to_yaml(temp, request.number)
-        
-        # Save data to .yaml
-        self.get_logger().info(f"Completed to save data to .yaml file")
-        
-        return response
+        if self.update_number < 4:
+            self.update_number += 1
+            self.get_logger().info(f"Update pizza position {self.update_number}")
+            
+            # Handle the incoming 1D array of float64
+            self.get_logger().info(f"Received pizza pose to save to pizza_pose.yaml")
+            
+            print(self.keep_pose, self.update_number)
+            
+            # Call the function to append data to YAML
+            self.append_to_yaml(self.keep_pose, self.update_number)
+            
+            # Save data to .yaml
+            self.get_logger().info(f"Completed to save data to .yaml file")
+            
+            self.keep_pose = []
+            
+            return response
+        else:
+            self.get_logger().warning(f"Can't update pizza position after {self.update_number} times")  
+            return response
     
     def clear_yaml(self, file_path):
         self.write_yaml(file_path, {})  # Clears the YAML file by overwriting with an empty dictionary
@@ -77,6 +120,15 @@ class PizzaViapointNode(Node):
         except Exception as e:
             self.get_logger().error(f"Failed to write to {file_path}: {str(e)}")
 
+    def on_parameter_update(self, params):
+        # Callback to handle parameter updates
+        for param in params:
+            if param.name == 'max_pizza':
+                self.max_pizza = param.value
+                self.get_logger().info(f'Update max pizza changed to: {self.max_pizza} pcs')
+
+        return SetParametersResult(successful=True)
+    
 def main(args=None):
     rclpy.init(args=args)
     node = PizzaViapointNode()
