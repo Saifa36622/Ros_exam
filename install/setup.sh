149a0ba027c8034ad8b3102dfd1413c5e264675f
SunnY <93282477+nakerin7588@users.noreply.github.com>
2024-09-14 11:41:05 +0700

    Update1

--- install/setup.sh
+++ install/setup.sh
@@ -7,7 +7,7 @@
 # since a plain shell script can't determine its own path when being sourced
 # either use the provided COLCON_CURRENT_PREFIX
 # or fall back to the build time prefix (if it exists)
-_colcon_prefix_chain_sh_COLCON_CURRENT_PREFIX=/home/saifa/Desktop/ros_exam_ws/install
+_colcon_prefix_chain_sh_COLCON_CURRENT_PREFIX=/home/nakarin/RoboticsDevExam1/install
 if [ ! -z "$COLCON_CURRENT_PREFIX" ]; then
   _colcon_prefix_chain_sh_COLCON_CURRENT_PREFIX="$COLCON_CURRENT_PREFIX"
 elif [ ! -d "$_colcon_prefix_chain_sh_COLCON_CURRENT_PREFIX" ]; then
@@ -35,7 +35,7 @@ COLCON_CURRENT_PREFIX="/opt/ros/humble"
 _colcon_prefix_chain_sh_source_script "$COLCON_CURRENT_PREFIX/local_setup.sh"
 
 # setting COLCON_CURRENT_PREFIX avoids relying on the build time prefix of the sourced script
-COLCON_CURRENT_PREFIX="/home/saifa/Desktop/ros2_w2_ws/install"
+COLCON_CURRENT_PREFIX="/home/nakarin/RoboticsDev/install"
 _colcon_prefix_chain_sh_source_script "$COLCON_CURRENT_PREFIX/local_setup.sh"
 
 
