096cb3499d92643865555d8e4f5d87d6fcd495c7
SunnY <93282477+nakerin7588@users.noreply.github.com>
2024-09-14 20:58:24 +0700

    Delete build install log

--- build/sun_interfaces/CMakeFiles/sun_interfaces.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/sun_interfaces"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/sun_interfaces.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/sun_pkg/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nakarin/RoboticsDevExam2/src/sun_pkg
-# Build directory: /home/nakarin/RoboticsDevExam2/build/sun_pkg
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
